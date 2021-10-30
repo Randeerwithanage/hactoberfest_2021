ccb9854534d0bacc3a9f684a40b0cade064f3f54
Randeer_withanage_99 <64647283+Randeerwithanage@users.noreply.github.com>
2021-10-30 14:39:28 +0530

    Create simple_Calculator.py
    create simple Calculator use python,,,

---
+++ simple_Calculator.py
@@ -0,0 +1,62 @@
+# <<<<<<<<<<<<<<..........make a simple calculator for beginers for the payton ..........>>>>>>>
+
+
+def add(x, y):          # <--------adds two numbers
+    return x + y
+
+
+def subtract(x, y):     # <---------subtracts two numbers
+    return x - y
+
+
+def multiply(x, y):     # <-------- multiplies two numbers
+    return x * y
+
+
+def divide(x, y):       # <-------- multiplies two numbers
+    return x / y
+
+# print to user.....
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+   
+   
+    choice = input("Enter choice(1/2/3/4): ")        # <----------take input from the user
+
+    
+    if choice in ('1', '2', '3', '4'):               #<---------- check if choice is one of the four options
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        # check if user wants another calculation
+
+
+
+        # break the while loop if answer is no
+
+
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+
+          break
+    
+    else:
+        print("Invalid Input")     #<_______----print invalid to user
